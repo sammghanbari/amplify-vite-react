c135dc6f4e19dc0b21e29f239d966dea27f9b695
sammghanbari <samm.ghanbari@gmail.com>
2024-08-04 19:09:33 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -22,6 +25,10 @@ function App() {
   }
   
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
+      
     <main>
       <h1>My ToDo's</h1>
       <button onClick={createTodo}>+ New</button>
@@ -44,7 +51,11 @@ function App() {
       <br/>
         Newly Updated by Developer to Check for Status Committs.
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+          
+      )}
+    </Authenticator>
   );
 }
 
